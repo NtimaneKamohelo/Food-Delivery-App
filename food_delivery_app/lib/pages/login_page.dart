9eb0000187b2b280f569b69b7a13a7d9dd9d38af
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2024-09-26 14:06:53 +0200

    created the login page

---
+++ food_delivery_app/lib/pages/login_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Logo
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+
+            //Message, app slogan
+            Text(
+              "Food Delivery App",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+
+            //Email Textfield
+          ],
+        ),
+      ),
+    );
+  }
+}
